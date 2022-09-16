018d72211fa24ac5db49a660a328a43726b773be
Abduqodir Musayev <abduqodirmusayeff@gmail.com>
2022-09-16 18:04:50 +0500

    Revert "first RESTful API project"
    This reverts commit acea4a83146655815e37d503bcef341cb612d1f7.

--- main.go
+++ main.go
@@ -1,32 +1,99 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	Id       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
 func main() {
-	http.HandleFunc("/add",add)
-http.HandleFunc("/search",search)
+	router := mux.NewRouter()
 
-	http.ListenAndServe(":3000",nil)
+	movies = append(movies, Movie{Id: "1234", Isbn: "123441235", Title: "Lucy", Director: &Director{Firstname: "Luc", Lastname: "Besson"}})
+	movies = append(movies, Movie{Id: "1235", Isbn: "346734523", Title: "sherlock", Director: &Director{Firstname: "Microft", Lastname: "holmes"}})
+
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovieById).Methods("GET")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting server at localhost:8080\n")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func search(res http.ResponseWriter, req *http.Request) {
-	source := req.FormValue("source")
-	q := req.FormValue("q")
-	
-	data := "/search?q=" + q + "&source=" + source
-	res.Write([]byte(data))
+func getMovies(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(movies)
 }
 
-func add(res http.ResponseWriter, req *http.Request) {
-	n1 := req.FormValue("n1")
-	intN1, _ := strconv.Atoi(n1) 
-	n2 := req.FormValue("n2")
-	intN2, _ := strconv.Atoi(n2)
+func getMovieById(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for _, movie := range movies {
+		if movie.Id == params["id"] {
+			json.NewEncoder(response).Encode(movie)
+			return
+		}
+	}
+}
 
-	data := intN1 + intN2
-	res.Write([]byte(fmt.Sprint(data)))
+func createMovie(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	smth := json.NewDecoder(request.Body).Decode(&movie)
+
+	fmt.Println("asdfasdf", smth)
+
+	movie.Id = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	json.NewEncoder(response).Encode(movies)
+}
+
+func updateMovie(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	var updatedMovie Movie
+	_ = json.NewDecoder(request.Body).Decode(&updatedMovie)
+	updatedMovie.Id = params["id"]
+
+	for idx, movie := range movies {
+		if movie.Id == params["id"] {
+			movies = append(movies[:idx], movies[idx+1:]...)
+			movies = append(movies, updatedMovie)
+			json.NewEncoder(response).Encode(movies)
+			return
+		}
+	}
+}
+
+func deleteMovie(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for idx, movie := range movies {
+		if movie.Id == params["id"] {
+			movies = append(movies[:idx], movies[idx+1:]...)
+			json.NewEncoder(response).Encode(movies)
+			return
+		}
+	}
 }
