654b1557da5b5ef653f49b6f5570fca841d23037
Abduqodir Musayev <abduqodirmusayeff@gmail.com>
2022-09-22 16:00:23 +0500

    cool example with JSON

--- main.go
+++ main.go
@@ -3,37 +3,91 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
 )
-type Data struct {
-	username string
-	password string
-	ID int64
+
+type Name struct {
+	Family   string
+	Personal string
 }
-type Object struct {
-	data Data
+type Email struct {
+	ID     int
+	Adress string
+}
+type Interest struct {
+	ID    int
+	Hobby string
+}
+type Person struct {
+	ID        int
+	FirstName string
+	Name      Name
+	Email     []Email
+	Interest  []Interest
+}
+
+func GetPerson(p *Person) string {
+	return p.FirstName + " ID: " + strconv.Itoa(p.ID)
+}
+
+func GetPersonEmailAddress(p *Person, i int) string {
+	return p.Email[i].Adress
+}
+
+func WriteMessage(msg string) {
+	fmt.Println(msg)
+}
+
+func WriteStars() {
+	fmt.Println("------------------------------------------------------------------------------")
+}
+
+func checkError(err error) {
+	if err != nil {
+		WriteMessage(err.Error())
+		os.Exit(1)
+	}
+}
+
+func SaveJson(fileName string, key interface{}) {
+	outFile, err := os.Create(fileName)
+	checkError(err)
+	encoder := json.NewEncoder(outFile)
+	err = encoder.Encode(key)
+	checkError(err)
+	defer outFile.Close()
 }
 func main() {
-	jsonString := `
-	{
-		"data": {
-			"username" : "jake",
-			"password" : "112233",
-			"ID" : 1
-		}
+	Person := Person{
+		ID:        1,
+		FirstName: "Abduqodir",
+		Name:      Name{Family: "Musayev", Personal: "Developer"},
+		Email: []Email{
+			Email{ID: 1, Adress: "abduqodirmusayeff@gmail.com"},
+			Email{ID: 2, Adress: "Abduqodirmusayev7059@gmail.com"},
+		},
+		Interest: []Interest{
+			Interest{ID: 1,Hobby: "Baseball"},
+			Interest{ID: 2, Hobby: "FootBall"},
+			Interest{ID: 3, Hobby: "Basketball"},
+		},
 	}
-	`
+	WriteMessage("Personal ishi boshlandi")
+	WriteStars()
+	res1 := GetPerson(&Person)
+	WriteMessage(res1)
+	WriteStars()
+	
+	WriteMessage("email boshlandi!!!")
+	WriteStars()
+	res2 := GetPersonEmailAddress(&Person, 0)
+	WriteMessage(res2)
+	WriteStars()
 
-	var m Object
-	if err := json.Unmarshal([]byte(jsonString), &m); err != nil {
-		panic(err)
-	}
-	fmt.Println(m.data)
-
-fmt.Println("---------------------------------------------------------------------------------------------------------------")
-
-	b, err := json.Marshal(m)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(b))
+	WriteMessage("Save Json Boshlandi:")
+	WriteStars()
+	SaveJson("Malumot.json",&Person)
+	WriteMessage("Malumot sifatida yozildi")
+	WriteStars()
 }
